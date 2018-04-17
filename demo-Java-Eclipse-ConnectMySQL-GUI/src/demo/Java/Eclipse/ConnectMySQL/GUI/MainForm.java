88cc1a0608e82d15048412ba2b93760d12cfc976
root <root@ub16-base>
2018-04-17 18:00:37 +0300

    Test Alpha 1.5

--- demo-Java-Eclipse-ConnectMySQL-GUI/src/demo/Java/Eclipse/ConnectMySQL/GUI/MainForm.java
+++ demo-Java-Eclipse-ConnectMySQL-GUI/src/demo/Java/Eclipse/ConnectMySQL/GUI/MainForm.java
@@ -9,10 +9,12 @@ import java.sql.*;
 
 public class MainForm {
 	private JFrame frame = new JFrame();
+	private JDialog alert = new JDialog();	
 	
-	JButton btnNewButton = new JButton("Show data");
+	JButton btnShowGridDataButton = new JButton("Show data");
+	JButton btnDeleteRowButton = new JButton("Delete row");
 	JEditorPane editorPane = new JEditorPane();	
-	JButton btnNewButton_1 = new JButton("Clear");
+	JButton btnClearGridButton = new JButton("Clear");
 	Connection con =null;     
     
     DefaultTableModel model = new DefaultTableModel();
@@ -40,15 +42,37 @@ public class MainForm {
 	}
 
 	private void initialize() {
+		
+		
+		
+		System.out.println("Test Connection to MySQL");
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded");
+			
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error loading jdbc driver");
+			e.printStackTrace();
+		}
+			
+		try {
+			con=DriverManager.getConnection(url, user, password);
+		} catch (SQLException e1) {			
+			e1.printStackTrace();
+		}
+		System.out.println("Connection to DB created");
+		
 		model.addColumn("Z1");
 		model.addColumn("Z2");
+		alert.setBounds(200, 100, 300, 100);
 		frame.setBounds(100, 100, 634, 534);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);	
 		
-		btnNewButton.setBounds(24, 464, 117, 25);
-		frame.getContentPane().add(btnNewButton);		
-		btnNewButton_1.addActionListener(new ActionListener() {
+		btnShowGridDataButton.setBounds(34, 335, 117, 25);
+		frame.getContentPane().add(btnShowGridDataButton);		
+		btnClearGridButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				editorPane.setText("Clear data from grid");				
 				int RowCount = model.getRowCount();			
@@ -60,37 +84,70 @@ public class MainForm {
 			}
 		});
 		
-		btnNewButton_1.setBounds(153, 464, 117, 25);
-		frame.getContentPane().add(btnNewButton_1);
+		btnClearGridButton.setBounds(163, 335, 117, 25);
+		frame.getContentPane().add(btnClearGridButton);
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				alert.setTitle("Mouse clicked");
+				alert.setVisible(true);				
+			}
+		});
 		
 		table.setBounds(24, 12, 583, 303);
 		frame.getContentPane().add(table);		
 		
-		editorPane.setBounds(24, 341, 583, 100);
-		frame.getContentPane().add(editorPane);
+		editorPane.setBounds(24, 389, 583, 100);
+		frame.getContentPane().add(editorPane);		
 		
-		btnNewButton.addActionListener(new ActionListener() {
-			
-			public void actionPerformed(ActionEvent arg0) {							
-				System.out.println("Test Connection to MySQL");
+		btnDeleteRowButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {					
+				//Delete selected record				
+				Statement statment;
 				try {
-					Class.forName("com.mysql.jdbc.Driver");
-					System.out.println("Driver loaded");
-				} catch (ClassNotFoundException e) {
-					System.out.println("Error loading jdbc driver");
+					statment = con.createStatement();										
+					String insertSQL = "delete from items where id = 1";			
+					statment.executeUpdate(insertSQL);
+					statment.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
+				alert.setTitle("Record deleted");
+				alert.setVisible(true);
+				System.out.println("Record deleted");
+			}
+		});
+		btnDeleteRowButton.setBounds(288, 335, 117, 25);
+		frame.getContentPane().add(btnDeleteRowButton);
+		
+		JButton btnInsertRowButton = new JButton("Insert");
+		btnInsertRowButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// Insert record
+				try {
+					Statement statment = con.createStatement();
+					System.out.println("Statement created");					
+					String insertSQL = "insert into items values (1, 'TEST-555')";			
+					statment.executeUpdate(insertSQL);
+					statment.close();
+				} catch (SQLException e) {					
 					e.printStackTrace();
 				}
-								
-				try {
-					con=DriverManager.getConnection(url, user, password);
-					System.out.println("Connection to DB created");
+				alert.setTitle("Record inserted");
+				alert.setVisible(true);
 					
+			}
+		});
+		btnInsertRowButton.setBounds(417, 335, 117, 25);
+		frame.getContentPane().add(btnInsertRowButton);
+		
+		btnShowGridDataButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent arg0) {	
+				try {					
 					Statement statment = con.createStatement();
 					System.out.println("Statement created");
-					
-					//String insertSQL = "insert into items values (1, 'TEST-555')";			
-					//statment.executeUpdate(insertSQL);		
-					
 					String sql;
 				    sql = "SELECT id, name FROM items";
 				    ResultSet rs = statment.executeQuery(sql);	 
@@ -104,7 +161,7 @@ public class MainForm {
 				    }		      
 				    rs.close();
 				    statment.close();
-				    con.close();
+				    //con.close();
 				      
 				} catch (SQLException e) {					
 					e.printStackTrace();
@@ -113,5 +170,4 @@ public class MainForm {
 		});		
 		
 	}
-	
 }
